7079f3827a37c6de721de8bb2d7ef5647b17a277
DESKTOP-MQLHU09\user <okcat09@gmail.com>
2020-07-23 14:12:09 +0700

    First Page

--- input_port.py
+++ input_port.py
@@ -5,6 +5,7 @@ from tkinter.ttk import Combobox
 from tkinter import messagebox
 
 
+
 poot = Tk()
 poot.title("Add connection")
     
@@ -18,10 +19,10 @@ label_port.place(x=50, y=50)
 
 list_come = ["COM1", "COM2", "COM3", "COM4","COM5", "COM6", "COM7","COM8", "COM9", "COM10", "COM11", "COM12"]
 combo = Combobox(poot, values = list_come )
-##port = Entry(poot, width=21) #port COM
 combo.place(x=200, y=50)
 
-
+  
+    
     
 ##    label_baudrate = Label(poot, text="baudrate", width=20, font=("arial",10,"bold"))
 ##    label_baudrate.place(x=50, y=100)
@@ -48,26 +49,35 @@ combo.place(x=200, y=50)
 ##    br = Entry(poot, width=21) #9600
 ##    br.place(x=250, y=250)
     
+##       
 
-
-def write_port():
+def check_write():
+    com = str(combo.get())
+    s = list_come
     
-    com= str(combo.get())
-    config = configparser.ConfigParser()
-    config["portt"] = {"numberport": com }
-    with open('connection.ini','w') as configfile:
-        config.write(configfile)
+    if com in s:
+        config = configparser.ConfigParser()
+        config["portt"] = {"numberport": com }
+        with open('connection.ini','w') as configfile:
+            config.write(configfile)
 
-    if(os.path.isfile('connection.ini')==True):
-        answer = messagebox.showinfo("Message","Write!!!")
+        if(os.path.isfile('connection.ini')==True):
+            answer = messagebox.showinfo("Message","Successful!!!")
+            poot.destroy()
+    else:
+        messagebox.showinfo("Message", "Please Enter The Message Again")
 
-        poot.destroy()
+   
+        
+def disable_event():
+    pass      
     
 
-b4= Button(poot, text="Submit", width=12,bg='brown',fg='white',command=write_port)
+b4= Button(poot, text="Submit", width=12,bg='brown',fg='white',command=check_write)
     
 b4.place(x=220,y=100)
 
+poot.protocol("WM_DELETE_WINDOW", disable_event)
     
 poot.mainloop()
 
